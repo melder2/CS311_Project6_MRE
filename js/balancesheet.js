d8819236e9b02edc09d29e38fcb41a98c1e83da6
Michael <mokelder4@gmail.com>
2022-12-06 13:23:46 -0500

    second commit

--- js/balancesheet.js
+++ js/balancesheet.js
@@ -1,8 +1,69 @@
-const AddCurrentAssets = document.getElementById('AddCurrentAssets');
-const CurrentAssets = document.getElementById('')
+document.getElementById("currentassetsform").style.display = "flex";
+
+
 
 function CurrentAssetsClick() {
-   document.getElementById("currentassetsform").style.display = "flex";
-   removeElementById("currentassetsbutton");
+   const addCurrentAssets = document.getElementById("currentassetsbutton");
+   addCurrentAssets.remove();
+   
+   const data = document.getElementById("currentassetsform");
+   
+   let box = document.createElement("div");
+   box.classList.add("form");
+   box.setAttribute('id', 'currentassets');
+   let ca = document.createElement("p");
+   text = document.createTextNode(data.value);
+   ca.appendChild(box);
+   box.appendChild(text);
+   
 
+
+
+
+   
+}
+
+function CurrentLiabilitiesClick() {
+   const addCurrentLiabilities = document.getElementById("currentliabilitiesbutton");
+   addCurrentLiabilities.remove();
+
+   
+
+   
+}
+
+function InvestmentPropertyClick() {
+   const addInvestmentProperty = document.getElementById("investmentpropertyandequipbutton");
+   addInvestmentProperty.remove();
+
+   
+
+   
+}
+
+function LongTermLiabilityClick() {
+   const addLongTermLiabilities = document.getElementById("AddLong-TermLiabilities");
+   addLongTermLiabilities.remove();
+
+   
+
+   
+}
+
+function IntangiblesClick() {
+   const addIntangibles = document.getElementById("AddIntangibles");
+   addIntangibles.remove();
+
+   
+
+   
+}
+
+function CalculatePositionClick() {
+   const addCalculatePosition = document.getElementById("calculateposition");
+   addCalculatePosition.remove();
+
+   
+
+   
 }
